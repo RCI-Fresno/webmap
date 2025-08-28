602c82f214520ebed63d77561f64aa2fa201f9cc
erikalew <erika@urbanfootprint.com>
2025-08-27 22:32:53 -0700

    Preliminary infill sites map
    Preliminary infill sites map

---
+++ data/Sitesunder1acrewhereadjacentparcelsundercommonownershiparemergedintosinglesites_8.js
@@ -0,0 +1,8 @@
+var json_Sitesunder1acrewhereadjacentparcelsundercommonownershiparemergedintosinglesites_8 = {
+"type": "FeatureCollection",
+"name": "Sitesunder1acrewhereadjacentparcelsundercommonownershiparemergedintosinglesites_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
